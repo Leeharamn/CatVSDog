c05f5ad0da956945ff2d9f02146db63312615a73
Leeharamn <167047523+Leeharamn@users.noreply.github.com>
2024-04-19 10:31:26 +0900

    lnit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public GameObject nomalCat;
+    public GameObject fatCat;
+    public GameObject pirateCat;
+    public GameObject retryBtn;
+
+    public RectTransform levelFront;
+    public Text levelTxt;
+
+    int level = 0;
+    int score = 0;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        Application.targetFrameRate = 60;
+        Time.timeScale = 1.0f;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("MakeCat", 0f, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void MakeCat()
+    {
+        Instantiate(nomalCat);
+
+        if (level == 1)
+        {
+            int p = Random.Range(0, 10);
+            if(p < 2)Instantiate(nomalCat);
+        }
+        else if (level == 2)
+        {
+            int p = Random.Range(0, 10);
+            if (p < 5) Instantiate(nomalCat);
+        }
+        else if (level == 3)
+        {
+            Instantiate(fatCat);
+        }
+        else if (level >= 4)
+        {
+            float p = Random.Range(0, 10);
+            if (p < 5) Instantiate(nomalCat);
+
+            Instantiate(fatCat);
+            Instantiate(pirateCat);
+        }
+    }
+
+    public void GameOver()
+    {
+        retryBtn.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void AddScore()
+    {
+        score++;
+        level = score / 5;
+        levelTxt.text = level.ToString();
+        levelFront.localScale = new Vector3 ((score - level * 5) / 5.0f, 1f, 1f);
+    }
+}
